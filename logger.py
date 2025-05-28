4406152a1a240b8c6f03d6f2ce3bb73c468a7de6
Anteros <110900508+TearLou@users.noreply.github.com>
2025-05-28 21:55:33 +0300

    да емае

--- logger.py
+++ logger.py
@@ -42,8 +42,4 @@ def index():
     with open("ip_log.txt", "a") as f:
         f.write(log_entry)
 
-    return "Hello! Your info has been logged."
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    return "Шо ты голова."
